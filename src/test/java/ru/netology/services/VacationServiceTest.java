4c925b74329b023bd51020fc11ea5fd4027671b5
Dora Charm <kovina.dora@gmail.com>
2023-04-29 09:43:50 +0500

    Task 2 Complete

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,30 +1,15 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void testLowIncome(){
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/income.csv")
+    public void testLowIncome(int expected, int income, int expenses, int threshold){
         VacationService service = new VacationService();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        int actual = service.calcVacation(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testHighIncome(){
-        VacationService service = new VacationService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
         int actual = service.calcVacation(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
